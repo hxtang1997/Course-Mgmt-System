11ebd041f7d5afa5ddbc3173f86600eccb71cd6e
Hui Xin <huixintang@Huis-MacBook-Pro.local>
2019-10-31 16:43:09 +0100

    creation of teacher header and class files

---
+++ Teacher.h
@@ -0,0 +1,33 @@
+#include <string>
+using namespace std;
+
+class Teacher {
+    private:
+    string id;
+    string name;
+    char gender;
+    int phone;
+
+    public:
+    Teacher();
+
+    Teacher(string id, string name, char gender, int phone);
+
+    string getName();
+
+    string getID();
+
+    char getGender();
+
+    int getPhone();
+
+    void setName(string name);
+
+    void setID(string id);
+
+    void setGender(char gender);
+
+    void setPhone(int phone);
+
+    string toString();
+};
--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <exception>
 #include "RecordManager.h"
 #include "Student.h"
+#include "Teacher.h"
 
 using namespace std;
 
@@ -15,61 +16,6 @@ struct CustomException : public std::exception
 	const char * what () const throw ()
     {
     	return "Error occurred!";
-    }
-};
-
-class Teacher {
-    private:
-    string id;
-    string name;
-    char gender;
-    int phone;
-
-    public:
-    Teacher(){}
-    Teacher(string id, string name, char gender, int phone){
-        this->id = id;
-        this->name = name;
-        this->gender = gender;
-        this->phone = phone;
-    }
-
-    string getName(){
-        return this->name;
-    }
-
-    string getID(){
-        return this->id;
-    }
-
-    char getGender(){
-        return this->gender;
-    }
-
-    int getPhone(){
-        return this->phone;
-    }
-
-    void setName(string name){
-        this->name = name;
-    }
-
-    void setID(string id){
-        this->id = id;
-    }
-
-    void setGender(char gender){
-        this->gender = gender;
-    }
-
-    void setPhone(int phone){
-        this->phone = phone;
-    }
-
-    string toString(){
-        string toReturn = this->id + "," + this->name + "," + this->gender + ",";
-        toReturn += to_string(this->phone);
-        return toReturn;
     }
 };
 
